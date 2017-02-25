3c9a67da3eb7cb80de57cfa8ca4913265d7c9857
Kushimayo <akstp31@naver.com>
2017-02-25 16:10:46 +0900

    create cube and stage
    create cube and floors..

---
+++ RollingCube/Assets/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    float KeyPressMoveVector = 0.1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * 100 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Vector3 moveLeft = new Vector3(0, 0, -KeyPressMoveVector);
+            //moveLeft = moveLeft.normalized;
+            //GetComponent<Rigidbody>().AddForce(moveLeft);
+            GetComponent<Rigidbody>().AddForce(Vector3.left);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right);
+        } else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+        }
+    }
+}
